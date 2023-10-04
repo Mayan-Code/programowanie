34072955ca15250636f05f4bcc5fea9d4a315c80
inlinecode <pawel.maj@optimakers.pl>
2023-10-04 23:04:50 +0200

    Zmiany dodane z commandline

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dodanie jako takiego tekstu");
-Console.WriteLine("Dodanie jako takiego tekstu2");
+Console.WriteLine("Dodanie jako takiego tekstu");
+Console.WriteLine("Dodanie jako takiego tekstu z command line");
 
 
 Console.ReadKey();
