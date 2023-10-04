3286ee22ec1e85e9bef829ebc98bb42b7ed24f68
inlinecode <pawel.maj@optimakers.pl>
2023-10-04 22:54:57 +0200

    Lekka modyfikaccja 2

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Dodanie jako takiego tekstu");
+Console.WriteLine("Dodanie jako takiego tekstu2");
 
 
 Console.ReadKey();
